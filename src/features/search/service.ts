8046e8a3cb6504b23d6096cb3a99451639ed8f59
ReinforceZwei <39955851+ReinforceZwei@users.noreply.github.com>
2025-12-31 12:01:38 +0800

    add serach sql

---
+++ src/features/search/service.ts
@@ -0,0 +1,53 @@
+import { ServiceBase } from "@/lib/service-base.class";
+import { Bookmark } from "@/generated/prisma/client";
+import { z } from "zod";
+
+const searchQuerySchema = z.object({
+  query: z.string(),
+  userId: z.string(),
+  limit: z.number().optional(),
+  offset: z.number().optional(),
+});
+
+export class SearchService extends ServiceBase {
+  async search(data: z.infer<typeof searchQuerySchema>) {
+    const validatedData = searchQuerySchema.parse(data);
+    const limit = validatedData.limit ?? 10;
+    const offset = validatedData.offset ?? 0;
+    return await this.prisma.$queryRaw<Bookmark[]>`
+SELECT
+	*
+FROM
+	"Bookmark"
+WHERE
+	(
+		SETWEIGHT(TO_TSVECTOR('english', COALESCE(TITLE, '')), 'A') || SETWEIGHT(
+			TO_TSVECTOR('simple', COALESCE(TRANSLATE(URL, '.', ' '), '')),
+			'B'
+		) || SETWEIGHT(
+			TO_TSVECTOR('english', COALESCE(DESCRIPTION, '')),
+			'C'
+		) @@ TO_TSQUERY(
+			'simple',
+			WEBSEARCH_TO_TSQUERY('simple', ${validatedData.query})::TEXT || ':*'
+		)
+	)
+	AND "userId" = ${validatedData.userId}
+ORDER BY
+	TS_RANK(
+		SETWEIGHT(TO_TSVECTOR('english', COALESCE(TITLE, '')), 'A') || SETWEIGHT(
+			TO_TSVECTOR('simple', COALESCE(TRANSLATE(URL, '.', ' '), '')),
+			'B'
+		) || SETWEIGHT(
+			TO_TSVECTOR('english', COALESCE(DESCRIPTION, '')),
+			'C'
+		),
+		TO_TSQUERY(
+			'simple',
+			WEBSEARCH_TO_TSQUERY('simple', ${validatedData.query})::TEXT || ':*'
+		)
+	) DESC
+LIMIT ${limit} OFFSET ${offset};
+    `
+  }
+}
